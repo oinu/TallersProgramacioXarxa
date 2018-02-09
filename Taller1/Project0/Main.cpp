546a36bb116224d9c15ea641eb8fbf761075710f
unknown <oinu69@gmail.com>
2018-02-09 21:50:28 +0100

    Hace la connexion servidor cliente, ahora se esta trabajando en que se haga en Threads

--- Taller1/Project0/Main.cpp
+++ Taller1/Project0/Main.cpp
@@ -1,73 +1,179 @@
 #include <SFML\Graphics.hpp>
 #include <SFML\Network.hpp>
 #include <string>
-#include <cstring>
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+#define MAX_MENSAJES 30
+#define SERVER_PORT 50000
+#define CLIENT_PORT 50000
+#define BUFFER_SIZE 2000
+
+
+using namespace std;
+std::mutex mu;
+
+//Escucha por un socket determinado y escribe el mensaje llegado.
+//socket: Es el socket al cual debemos escuchar.
+//recived: La longitud de los datos que recibiremos.
+//aMensajes: Es donde contendra todos los mensajes del chat.
+void RecivedFunction(sf::TcpSocket* socket,size_t* recived, vector<string>* aMensajes)
+{
+	mu.lock();
+	char buffer[BUFFER_SIZE];
+	sf::Socket::Status status= socket->receive(buffer, sizeof(buffer), *recived);
+	string s = buffer;
+	if (status != sf::Socket::Status::Disconnected)
+	{
+		aMensajes->push_back(s);
+		if (aMensajes->size() > 25)
+		{
+			aMensajes->erase(aMensajes->begin(), aMensajes->begin() + 1);
+		}
+	}
+	mu.unlock();
+}
 
 int main()
 {
-	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	//ESTABLECER CONNECION
 	sf::TcpSocket socket;
-	char connectionType, mode;
-	char buffer[2000];
-	std::size_t received;
-	std::string text = "Connected to: ";
+	char connectionType;
+	size_t recived;
 
-	std::cout << "Enter (s) for Server, Enter (c) for Client: ";
-	std::cin >> connectionType;
-
+	cout << "Introduce (s) para Server o (c) para Cliente:";
+	cin >> connectionType;
+	
+	// Si es el servidor
 	if (connectionType == 's')
 	{
+		//Nos ponemos en escucha en el puerto indicado
 		sf::TcpListener listener;
-		listener.listen(5000);
+		listener.listen(SERVER_PORT);
+
+		//Esperamos peticion de un cliente
 		listener.accept(socket);
-		text += "Server";
-		mode = 's';
+		
+		//Cuando tenemos una coneccion con un cliente, cerramos la escucha.
 		listener.close();
 	}
+
+	//Si es el cliente
 	else if (connectionType == 'c')
 	{
-		socket.connect(ip, 5000);
-		text += "Client";
-		mode = 'r';
+		// Obtenemos nuestra direccion ip, y nos connectamos con el puerto indicado y nuestra ip
+		sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+		socket.connect(ip, CLIENT_PORT);
 	}
 
-	socket.send(text.c_str(), text.length() + 1);
-	socket.receive(buffer, sizeof(buffer), received);
+	std::vector<std::string> aMensajes;
 
-	std::cout << buffer << std::endl;
+	sf::Vector2i screenDimensions(800, 600);
 
-	bool done = false;
-	while (!done)
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+
+	sf::Font font;
+	if (!font.loadFromFile("Roboto-Bold.ttf"))
 	{
-		if (mode == 's')
+		std::cout << "Can't load the font file" << std::endl;
+	}
+
+	sf::String mensaje = " >";
+
+	sf::Text chattingText(mensaje, font, 24);
+	chattingText.setFillColor(sf::Color(0, 160, 0));
+	chattingText.setStyle(sf::Text::Bold);
+
+
+	sf::Text text(mensaje, font, 24);
+	text.setFillColor(sf::Color(0, 160, 0));
+	text.setStyle(sf::Text::Bold);
+	text.setPosition(0, 560);
+
+	sf::RectangleShape separator(sf::Vector2f(800, 5));
+	separator.setFillColor(sf::Color(200, 200, 200, 255));
+	separator.setPosition(0, 550);
+
+	string msn;
+	thread t(RecivedFunction, &socket, &recived, &aMensajes);
+
+	while (window.isOpen())
+	{
+		sf::Event evento;
+		while (window.pollEvent(evento))
 		{
-			std::getline(std::cin, text);
-			if (text.length() > 0)
+			switch (evento.type)
 			{
-				socket.send(text.c_str(), text.length() + 1);
-				mode = 'r';
-				if (text == "exit")
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (evento.key.code == sf::Keyboard::Escape)
+					window.close();
+				else if (evento.key.code == sf::Keyboard::Return)
 				{
-					break;
+					aMensajes.push_back(mensaje);
+					if (aMensajes.size() > 25)
+					{
+						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+					}
+					// SEND
+					// Pasamos el mensaje a std::string para hacerlo mas facil en el momento de enviarlo.
+					msn = mensaje;
+					socket.send(msn.c_str(), msn.size() + 1);
+
+					mensaje = ">";
 				}
+				break;
+			case sf::Event::TextEntered:
+				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+					mensaje += (char)evento.text.unicode;
+				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+				break;
 			}
 		}
-		else if (mode == 'r')
+		
+		//RECIVE
+		/*if (connectionType == 's')
 		{
-			socket.receive(buffer, sizeof(buffer), received);
-			if (received > 0)
+			string s = RecivedFunction(&socket, &recived);
+			if (s != "")
 			{
-				std::cout << "Received: " << buffer << std::endl;
-				mode = 's';
-				if (strcmp(buffer, "exit") == 0)
+				aMensajes.push_back(s);
+				if (aMensajes.size() > 25)
 				{
-					break;
+					aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
 				}
 			}
+			
+		}*/
+
+		//SE TIENE QUE LAZAR UN THREAD CADA VEZ QUE UN TERMINA?
+		//std::thread t(RecivedFunction,&socket, &recived,&aMensajes);
+		
+		//t.join();
+
+		window.draw(separator);
+		for (size_t i = 0; i < aMensajes.size(); i++)
+		{
+			std::string chatting = aMensajes[i];
+			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+			chattingText.setString(chatting);
+			window.draw(chattingText);
 		}
+		std::string mensaje_ = mensaje + "_";
+		text.setString(mensaje_);
+		window.draw(text);
+
+
+		window.display();
+		window.clear();
 	}
 
 	socket.disconnect();
-	return 0;
+
 }
