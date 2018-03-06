8406756d459e4968f69411c0454fe9ad2621baca
Eloi Salles Torres <esalles@enti.local>
2018-03-06 18:15:41 +0100

    Cambio a uso de Packets para que la tarea sea mas facil de hacer.
    Signed-off-by: Eloi Salles Torres <esalles@enti.local>

--- Taller4/Bootstrap/main.cpp
+++ Taller4/Bootstrap/main.cpp
@@ -12,12 +12,37 @@
 
 
 using namespace std;
+struct Direccion
+{
+	std::string ip;
+	unsigned short port;
+
+	Direccion::Direccion(std::string aIp, unsigned short aPort)
+	{
+		ip = aIp;
+		port = aPort;
+	}
+};
+
+sf::Packet ParseToPacket(vector<Direccion> d)
+{
+	sf::Packet send;
+
+	for (Direccion dir : d)
+	{
+		send << dir.ip << dir.port;
+	}
+
+	return send;
+}
 
 int main()
 {
 	//Creamos el listener que esperara a 4 clientes, por su puerto correspondiente.
 	sf::TcpListener listener;
 	listener.listen(SERVER_PORT);
+	vector<Direccion> dList;
+
 
 	//Esperamos las cuatro connexiones.
 	for (int i = 0; i < 4; i++)
@@ -25,8 +50,26 @@ int main()
 		//Creamos un socket y escuchamos 
 		sf::TcpSocket socket;
 		listener.accept(socket);
+		if (i != 0)
+		{
+			sf::Packet p;
+
+			//Indicamos el numero de paquetes que recibira
+			p << dList.size();
+			socket.send(p);
+
+			//Enviamos tantos paquetes como direcciones conocemos
+			for (int j = 0; j < dList.size(); j++)
+			{
+				p.clear();
+				p << dList[j].ip << dList[j].port;
+				socket.send(p);
+			}			
+		}
 		//posarlo a un vector de la estructura direccio, que conte la ip i el port.
-		//informar s'ha connectat un client.
+		Direccion d(socket.getRemoteAddress, socket.getRemotePort);
+		dList.push_back(d);
+		
 		socket.disconnect();
 	}
 
---
+++ Taller4/Peer/main.cpp
@@ -0,0 +1,219 @@
+#include <SFML\Graphics.hpp>
+#include <SFML\Network.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+#define MAX_MENSAJES 30
+#define SERVER_PORT 50000
+#define CLIENT_PORT 50000
+#define BUFFER_SIZE 2000
+
+
+using namespace std;
+
+struct Direccion
+{
+	std::string ip;
+	unsigned short port;
+
+	Direccion::Direccion()
+	{
+
+	}
+	Direccion::Direccion(std::string aIp, unsigned short aPort)
+	{
+		ip = aIp;
+		port = aPort;
+	}
+};
+
+vector<Direccion> ParseToDireccion(sf::Packet p, int length)
+{
+}
+
+void RecivedFunction(vector<sf::TcpSocket*> socket, vector<string>* aMensajes, sf::SocketSelector* ss)
+{
+	char buffer[BUFFER_SIZE];
+	string msn;
+	size_t recived;
+	while (ss->wait())
+	{
+		for (int i = 0; i < socket.size(); i++)
+		{
+			if (ss->isReady(*socket[i]))
+			{
+				sf::Socket::Status st = socket[i]->receive(buffer, sizeof(buffer), recived);
+				if (st == sf::Socket::Status::Done)
+				{
+					msn = buffer;
+					aMensajes->push_back(msn);
+					if (aMensajes->size() > 25)
+					{
+						aMensajes->erase(aMensajes->begin(), aMensajes->begin() + 1);
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	//ESTABLECER CONNECION
+	sf::TcpSocket socket;
+	sf::SocketSelector ss;
+
+	char connectionType;
+	char buffer[BUFFER_SIZE];
+	size_t recived, sended;
+	sf::Socket::Status socketStatus;
+	sf::Packet p;
+
+	vector<Direccion> direccionList;
+
+	// Obtenemos nuestra direccion ip, y nos connectamos con el puerto indicado y nuestra ip
+	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+	socket.connect(ip, CLIENT_PORT);
+
+	//Recibir del servidor la lista de usuarios
+	socketStatus = socket.receive(p);
+	if (socketStatus != sf::TcpSocket::Status::Done)
+	{
+		cout << "Error" << endl;
+	}
+	else
+	{
+		//Obtenemos cuantos paquetes vamos a esperar
+		int lenght;
+		p >> lenght;
+
+		//Si es 0, quiere decir que somos los primeros
+		if (lenght != 0)
+		{
+			//Si no somos los primeros, reciviremos tantos paquetes como peers conocidos
+			for (int j = 0; j < lenght; j++)
+			{
+				//Recivimos los packetes
+				socketStatus = socket.receive(p);
+
+				//Comprovamos que no haya error
+				if (socketStatus != sf::TcpSocket::Status::Done)
+				{
+					cout << "Error";
+				}
+				else
+				{
+					//Creamos una direccion
+					Direccion d;
+
+					//Obtenemos su ip y puerto
+					p >> d.ip >> d.port;
+
+					//Lo colocamos a la list.
+					direccionList.push_back(d);
+				}
+			}
+		}
+	}
+
+	//----------------------------------//
+	std::vector<std::string> aMensajes;
+
+	sf::Vector2i screenDimensions(800, 600);
+
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
+
+	sf::Font font;
+	if (!font.loadFromFile("Roboto-Bold.ttf"))
+	{
+		std::cout << "Can't load the font file" << std::endl;
+	}
+
+	sf::String mensaje = " >";
+
+	sf::Text chattingText(mensaje, font, 24);
+	chattingText.setFillColor(sf::Color(0, 160, 0));
+	chattingText.setStyle(sf::Text::Bold);
+
+
+	sf::Text text(mensaje, font, 24);
+	text.setFillColor(sf::Color(0, 160, 0));
+	text.setStyle(sf::Text::Bold);
+	text.setPosition(0, 560);
+
+	sf::RectangleShape separator(sf::Vector2f(800, 5));
+	separator.setFillColor(sf::Color(200, 200, 200, 255));
+	separator.setPosition(0, 550);
+
+	string msn;
+	thread t(RecivedFunction,&socket, &aMensajes, &ss);
+
+	while (window.isOpen())
+	{
+		sf::Event evento;
+		while (window.pollEvent(evento))
+		{
+			switch (evento.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (evento.key.code == sf::Keyboard::Escape)
+					window.close();
+				else if (evento.key.code == sf::Keyboard::Return)
+				{
+					aMensajes.push_back(mensaje);
+					if (aMensajes.size() > 25)
+					{
+						aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+					}
+					// SEND
+					// Pasamos el mensaje a std::string para hacerlo mas facil en el momento de enviarlo.
+					msn = mensaje;
+
+					//Se tiene que enviar a cada Peer
+					socket.send(msn.c_str(), msn.size() + 1);
+
+					mensaje = ">";
+				}
+				break;
+			case sf::Event::TextEntered:
+				if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+					mensaje += (char)evento.text.unicode;
+				else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+					mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+				break;
+			}
+		}
+
+		
+		window.draw(separator);
+		for (size_t i = 0; i < aMensajes.size(); i++)
+		{
+			std::string chatting = aMensajes[i];
+			chattingText.setPosition(sf::Vector2f(0, 20 * i));
+			chattingText.setString(chatting);
+			window.draw(chattingText);
+		}
+		std::string mensaje_ = mensaje + "_";
+		text.setString(mensaje_);
+		window.draw(text);
+
+
+		window.display();
+		window.clear();
+	}
+
+	//Acabar el Thread
+	ss.clear();
+	t.join();
+	
+
+	socket.disconnect();
+
+}
